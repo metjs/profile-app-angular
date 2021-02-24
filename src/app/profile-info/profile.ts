968dea89220c1e8320fdb9f31b7beb59af080c47
Mehmet Eren Toper <mehmeterentoper@gmail.com>
2021-02-24 15:37:59 +0300

    + added class for type safe

---
+++ src/app/profile-info/profile.ts
@@ -0,0 +1,10 @@
+export class Profile {
+    id:number;
+    name:string;
+    image:string;
+    bio:string;
+    location:string;
+    birtday:string; //temp ..
+    username:string;
+
+}
---
+++ src/app/tweet-area/tweet.ts
@@ -0,0 +1,7 @@
+import { Time } from "@angular/common";
+
+export class Tweet {
+    id:number;
+    content:string;
+    date:string; //temp..
+}
