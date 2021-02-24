c8c2b26ef5fbb5930c1477ee4fc71e647a37a307
Mehmet Eren Toper <mehmeterentoper@gmail.com>
2021-02-24 21:07:09 +0300

    + added httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileInfoComponent } from './profile-info/profile-info.component';
 import { TweetAreaComponent } from './tweet-area/tweet-area.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileInfoComponent,
-    TweetAreaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProfileInfoComponent, TweetAreaComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
